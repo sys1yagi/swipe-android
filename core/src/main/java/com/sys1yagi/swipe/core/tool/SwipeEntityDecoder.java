58208b9c06f6d38029ac88278d6a0c117a83fd0c
sys1yagi <sylc.yagi@gmail.com>
2015-12-23 09:46:49 +0900

    Use hjson on SwipeEntityDecoder.

--- core/src/main/java/com/sys1yagi/swipe/core/tool/SwipeEntityDecoder.java
+++ core/src/main/java/com/sys1yagi/swipe/core/tool/SwipeEntityDecoder.java
@@ -6,14 +6,22 @@ import com.google.gson.JsonParser;
 import com.sys1yagi.swipe.core.entity.index.Index;
 import com.sys1yagi.swipe.core.entity.swipe.SwipeDocument;
 
+import org.hjson.JsonValue;
+
 public class SwipeEntityDecoder {
 
     private final static String KEY_TYPE = "type";
 
     private final static String TYPE_INDEX = "net.swipe.list";
 
+    //TODO remove it
+    private String hjson(String jsonString) {
+        return JsonValue.readHjson(jsonString).asObject().toString();
+    }
+
     public Index decodeToIndex(Gson gson, String jsonString) {
-        JsonObject json = new JsonParser().parse(jsonString).getAsJsonObject();
+
+        JsonObject json = new JsonParser().parse(hjson(jsonString)).getAsJsonObject();
         if (!json.has(KEY_TYPE)) {
             throw new IllegalArgumentException("Invalid index swipe file. data:" + jsonString);
         }
@@ -24,7 +32,8 @@ public class SwipeEntityDecoder {
     }
 
     public SwipeDocument decodeToSwipe(Gson gson, String jsonString) {
-        JsonObject json = new JsonParser().parse(jsonString).getAsJsonObject();
+
+        JsonObject json = new JsonParser().parse(hjson(jsonString)).getAsJsonObject();
         if (json.has(KEY_TYPE) && TYPE_INDEX.equals(json.get(KEY_TYPE).getAsString())) {
             throw new IllegalArgumentException("Invalid contents swipe file. data:" + jsonString);
         }
